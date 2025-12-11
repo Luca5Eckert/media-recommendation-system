a68ab0bab2fcc472892db08852868e18e78aa6a6
Luca5Eckert <ecklukatv@gmail.com>
2025-12-10 21:48:29 -0300

    fix: adiciona anotação para EngagementRepositoryAdapter

--- engagement-service/src/main/java/com/mrs/engagement_service/infrastructure/repository/EngagementRepositoryAdapter.java
+++ engagement-service/src/main/java/com/mrs/engagement_service/infrastructure/repository/EngagementRepositoryAdapter.java
@@ -2,7 +2,9 @@ package com.mrs.engagement_service.infrastructure.repository;
 
 import com.mrs.engagement_service.model.Interaction;
 import com.mrs.engagement_service.repository.EngagementRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EngagementRepositoryAdapter implements EngagementRepository {
 
     public final EngagementRepositoryJpa engagementRepositoryJpa;
