22d7451e585bdff877765f613ec3059a9c0c5893
Luca5Eckert <ecklukatv@gmail.com>
2025-12-11 16:56:59 -0300

    chore: corrigi depedencias dos microserviços

--- engagement-service/src/main/java/com/mrs/engagement_service/model/Interaction.java
+++ engagement-service/src/main/java/com/mrs/engagement_service/model/Interaction.java
@@ -3,7 +3,6 @@ package com.mrs.engagement_service.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
