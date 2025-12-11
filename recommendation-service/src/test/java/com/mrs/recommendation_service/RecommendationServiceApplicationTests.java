6aac4d99676f6693265daabdb94c7efcea132394
Luca5Eckert <ecklukatv@gmail.com>
2025-12-11 16:02:34 -0300

    chore: adiciona micro serviço de recomendação

---
+++ recommendation-service/src/test/java/com/mrs/recommendation_service/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrs.recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
