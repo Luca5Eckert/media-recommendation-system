45d0333ef2811830df8e2b31a0b1c9e4c196da12
Luca5Eckert <ecklukatv@gmail.com>
2025-12-11 13:45:48 -0300

    fix: adiciona validação do corpo da requisição para criar requisição

--- engagement-service/src/main/java/com/mrs/engagement_service/controller/EngagementController.java
+++ engagement-service/src/main/java/com/mrs/engagement_service/controller/EngagementController.java
@@ -2,6 +2,7 @@ package com.mrs.engagement_service.controller;
 
 import com.mrs.engagement_service.dto.InteractionCreateRequest;
 import com.mrs.engagement_service.service.EngagementService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ public class EngagementController {
     }
 
     @PostMapping
-    public ResponseEntity<String> create(@RequestBody InteractionCreateRequest engagement){
+    public ResponseEntity<String> create(@RequestBody @Valid InteractionCreateRequest engagement){
         engagementService.create(engagement);
 
         return ResponseEntity.status(HttpStatus.CREATED).body("Engagement registered with success");
--- engagement-service/src/main/java/com/mrs/engagement_service/dto/InteractionCreateRequest.java
+++ engagement-service/src/main/java/com/mrs/engagement_service/dto/InteractionCreateRequest.java
@@ -1,12 +1,13 @@
 package com.mrs.engagement_service.dto;
 
 import com.mrs.engagement_service.model.InteractionType;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.UUID;
 
 public record InteractionCreateRequest(
-        UUID userId,
-        UUID movieId,
-        InteractionType type
+        @NotBlank UUID userId,
+        @NotBlank UUID movieId,
+        @NotBlank InteractionType type
 ) {
 }
