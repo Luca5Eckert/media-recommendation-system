9f426cf065b90e173d8ee5dc2c7eb90a5cae698d
Luca5Eckert <ecklukatv@gmail.com>
2025-12-12 16:40:26 -0300

    fix: altera para modo nativo de conversão em array de text do Hibernate

--- recommendation-service/src/main/java/com/mrs/recommendation_service/model/MediaFeature.java
+++ recommendation-service/src/main/java/com/mrs/recommendation_service/model/MediaFeature.java
@@ -1,13 +1,13 @@
 package com.mrs.recommendation_service.model;
 
 
-import io.hypersistence.utils.hibernate.type.array.ListArrayType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-import org.hibernate.annotations.Type;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,7 +20,7 @@ public class MediaFeature {
     @Id
     private UUID mediaId;
 
-    @Type(ListArrayType.class)
+    @JdbcTypeCode(SqlTypes.ARRAY)
     @Column(name = "genres", columnDefinition = "text[]")
     private List<String> genres;
 
