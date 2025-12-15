41947c370f32e6f2b6b79eef3fe230c74f8234c8
Luca5Eckert <ecklukatv@gmail.com>
2025-12-15 18:52:52 -0300

    feat: adiciona caso de uso de deletar usuário

---
+++ user-service/src/main/java/com/mrs/user_service/handler/DeleteUserHandler.java
@@ -0,0 +1,25 @@
+package com.mrs.user_service.handler;
+
+import com.mrs.user_service.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class DeleteUserHandler {
+
+    private final UserRepository userRepository;
+
+    public DeleteUserHandler(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void handler(UUID userId){
+        if(!userRepository.existsById(userId)) {
+            throw new IllegalArgumentException("User not exist");
+        }
+
+        userRepository.deleteById(userId);
+    }
+
+}
