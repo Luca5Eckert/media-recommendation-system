3f5d75fcbb829295cfb3c50ec9f354c67a73f31a
Luca5Eckert <ecklukatv@gmail.com>
2025-12-16 18:30:59 -0300

    feat: implementa user details

--- user-service/src/main/java/com/mrs/user_service/security/user/UserDetailImpl.java
+++ user-service/src/main/java/com/mrs/user_service/security/user/UserDetailImpl.java
@@ -1,4 +1,37 @@
 package com.mrs.user_service.security.user;
 
-public class UserDetailImpl implements UserDE{
+import com.mrs.user_service.model.RoleUser;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailImpl implements UserDetails {
+
+    private final String email;
+    private final String password;
+    private final RoleUser role;
+
+    public UserDetailImpl(String email, String password, RoleUser role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
