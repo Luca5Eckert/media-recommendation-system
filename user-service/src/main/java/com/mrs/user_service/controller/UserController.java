b9d7a17a1d6cee909ff8cdc963e88d58694a4833
Luca5Eckert <ecklukatv@gmail.com>
2025-12-16 18:22:55 -0300

    feat: adiciona controller completo

--- user-service/src/main/java/com/mrs/user_service/controller/UserController.java
+++ user-service/src/main/java/com/mrs/user_service/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.mrs.user_service.controller;
 
 import com.mrs.user_service.dto.CreateUserRequest;
+import com.mrs.user_service.dto.UpdateUserRequest;
+import com.mrs.user_service.dto.UserGetResponse;
 import com.mrs.user_service.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -21,11 +23,36 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<String> create(@RequestBody @Valid CreateUserRequest createUserRequest){
+    public ResponseEntity<Void> create(@RequestBody @Valid CreateUserRequest createUserRequest) {
         userService.create(createUserRequest);
-
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body("User created with success");
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserGetResponse> getById(@PathVariable UUID id) {
+        UserGetResponse user = userService.get(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserGetResponse>> getAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        List<UserGetResponse> users = userService.getAll(page, size);
+        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(
+            @PathVariable UUID id,
+            @RequestBody @Valid UpdateUserRequest updateUserRequest) {
+        userService.update(id, updateUserRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
