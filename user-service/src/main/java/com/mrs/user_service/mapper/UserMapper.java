ea6f7ae93e55e004870d8899df067f6962216d38
Luca5Eckert <ecklukatv@gmail.com>
2025-12-15 21:17:44 -0300

    fix: adiciona anotação de component para mapper

--- user-service/src/main/java/com/mrs/user_service/mapper/UserMapper.java
+++ user-service/src/main/java/com/mrs/user_service/mapper/UserMapper.java
@@ -2,7 +2,9 @@ package com.mrs.user_service.mapper;
 
 import com.mrs.user_service.dto.UserGetResponse;
 import com.mrs.user_service.model.UserEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserMapper {
 
     public UserGetResponse toGetResponse(UserEntity user){
