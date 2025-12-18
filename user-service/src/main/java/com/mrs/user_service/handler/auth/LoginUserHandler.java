a49d04baf9f8117c7e5e8b95f68bfab751c42d06
Luca5Eckert <ecklukatv@gmail.com>
2025-12-17 21:39:40 -0300

    feat: adiciona login de usuário

--- user-service/src/main/java/com/mrs/user_service/handler/auth/LoginUserHandler.java
+++ user-service/src/main/java/com/mrs/user_service/handler/auth/LoginUserHandler.java
@@ -1,8 +1,34 @@
 package com.mrs.user_service.handler.auth;
 
+import com.mrs.user_service.security.token.TokenProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoginUserHandler {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenProvider tokenProvider;
+
+    public LoginUserHandler(AuthenticationManager authenticationManager, TokenProvider tokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;
+    }
+
+    public String execute(String email, String password) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        return tokenProvider.createToken(
+                userDetails.getUsername(),
+                userDetails.getAuthorities()
+        );
+    }
 
 }
