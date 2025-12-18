9148de703c68664d20f167e91fec049e5003bf43
Luca5Eckert <ecklukatv@gmail.com>
2025-12-17 21:40:06 -0300

    refactor: altera criação de token para suportar lista de GrantAuthority

--- user-service/src/main/java/com/mrs/user_service/security/token/JwtTokenProvider.java
+++ user-service/src/main/java/com/mrs/user_service/security/token/JwtTokenProvider.java
@@ -6,9 +6,11 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -28,13 +30,17 @@ public class JwtTokenProvider implements TokenProvider {
     }
 
     @Override
-    public String createToken(String email, RoleUser roleUser) {
+    public String createToken(String email, Collection<? extends GrantedAuthority> grantedAuthorities) {
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
 
+        List<String> roles = grantedAuthorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
         return Jwts.builder()
                 .setSubject(email)
-                .claim("roles", List.of(roleUser.getAuthority()))
+                .claim("roles", roles)
                 .setIssuedAt(now)
                 .setExpiration(validity)
                 .signWith(key, SignatureAlgorithm.HS256)
--- user-service/src/main/java/com/mrs/user_service/security/token/TokenProvider.java
+++ user-service/src/main/java/com/mrs/user_service/security/token/TokenProvider.java
@@ -3,9 +3,12 @@ package com.mrs.user_service.security.token;
 import com.mrs.user_service.model.RoleUser;
 import org.springframework.security.core.Authentication;
 import jakarta.servlet.http.HttpServletRequest; // Ou javax, dependendo da versão do Spring
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
 
 public interface TokenProvider {
 
-    String createToken(String email, RoleUser roleUser);
+    String createToken(String email, Collection<? extends GrantedAuthority> grantedAuthorities);
 
 }
