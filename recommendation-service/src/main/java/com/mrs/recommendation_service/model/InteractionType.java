34dc2b44411bcef590bdbd305a3294fdc9fef3ed
Luca5Eckert <ecklukatv@gmail.com>
2025-12-19 12:19:26 -0300

    #6 feat: adiciona peso por interação

--- recommendation-service/src/main/java/com/mrs/recommendation_service/model/InteractionType.java
+++ recommendation-service/src/main/java/com/mrs/recommendation_service/model/InteractionType.java
@@ -1,8 +1,18 @@
 package com.mrs.recommendation_service.model;
 
 public enum InteractionType {
-    LIKE,
-    DISLIKE,
-    WATCH
+    LIKE(2),
+    DISLIKE(-2),
+    WATCH(0.75);
+
+    double weightInteraction;
+
+    InteractionType(double weightInteraction){
+        this.weightInteraction = weightInteraction;
+    }
+
+    public double getWeightInteraction(){
+        return weightInteraction;
+    }
 
 }
