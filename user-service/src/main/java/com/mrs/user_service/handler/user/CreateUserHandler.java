6a730de77c55c01f4263ea70faad9d0de93aa079
Luca5Eckert <ecklukatv@gmail.com>
2025-12-24 00:33:07 -0300

    fix(user-service): add encode of password
    - Add password encoder in dependency
    - Add encode password in method

--- user-service/src/main/java/com/mrs/user_service/handler/user/CreateUserHandler.java
+++ user-service/src/main/java/com/mrs/user_service/handler/user/CreateUserHandler.java
@@ -1,28 +1,30 @@
 package com.mrs.user_service.handler.user;
 
 import com.mrs.user_service.model.UserEntity;
-import com.mrs.user_service.repository.UserRepository;
-import org.springframework.transaction.annotation.Transactional;
+import com.mrs. user_service.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class CreateUserHandler {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public CreateUserHandler(UserRepository userRepository) {
+    public CreateUserHandler(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this. passwordEncoder = passwordEncoder;
     }
 
     @Transactional
-    public void execute(UserEntity user){
-        if(user == null) throw new IllegalArgumentException("User can't be null");
-
-        if(userRepository.existsByEmail(user.getEmail())){
-            throw new IllegalArgumentException("User's email already in use");
+    public void execute(UserEntity user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("Email already in use");
         }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         userRepository.save(user);
     }
-
 }
